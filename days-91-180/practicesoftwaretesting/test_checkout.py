63bd282b4282e650e1ccf4f1f9b4d764b5b50330
Alexander <pkhaladze.alex@gmail.com>
2026-08-21 19:41:51 +0200

    Fix global CI import routing via PYTHONPATH and module init files

--- days-91-180/practicesoftwaretesting/test_checkout.py
+++ days-91-180/practicesoftwaretesting/test_checkout.py
@@ -1,8 +1,8 @@
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 
-from .config import *
-from .utils import *
+from config import *
+from utils import *
 def test_successful_guest_checkout(browser):
 
     wait = WebDriverWait(browser, IMPLICIT_WAIT_TIMEOUT)
--- days-91-180/practicesoftwaretesting/utils.py
+++ days-91-180/practicesoftwaretesting/utils.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-from .config import * 
+from config import *
+
 def add_product_to_cart(driver, wait):
     # Select a product
     first_product = wait.until(
